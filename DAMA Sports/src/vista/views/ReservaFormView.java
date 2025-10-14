718822a8c8bc6a7ae7e52bd0b9d78ce2985d7f27
K1ng-Yike567 <hugaroplay@gmail.com>
2025-10-14 13:13:55 +0200

    ImplementacionReserva

--- DAMA Sports/src/vista/views/ReservaFormView.java
+++ DAMA Sports/src/vista/views/ReservaFormView.java
@@ -15,8 +15,8 @@ public class ReservaFormView extends GridPane {
         setHgap(8); setVgap(8);
 
         TextField id = new TextField();
-        ComboBox<Socio> idSocio = new ComboBox();
-        ComboBox<Pista> idPista = new ComboBox();
+        ComboBox<String> idSocio = new ComboBox();
+        ComboBox<String> idPista = new ComboBox();
         DatePicker fecha = new DatePicker(LocalDate.now());
         TextField hora = new TextField("10:00");
         Spinner<Integer> duracion = new Spinner<>(30, 300, 60, 30);
@@ -32,13 +32,22 @@ public class ReservaFormView extends GridPane {
         addRow(6, new Label("Precio (€)"), precio);
         add(crear, 1, 7);
 
+        for (Socio s :  club.getSocios()) {
+            idSocio.getItems().add(s.getIdSocio());
+        }
+
+        for (Pista pista : club.getPistas()) {
+            idPista.getItems().add(pista.getIdPista());
+        }
+
         crear.setOnAction(e -> {
             try {
                 LocalTime t = LocalTime.parse(hora.getText());
 
-              Reserva r = new Reserva(id.getText(), idSocio.getValue().getIdSocio(), idPista.getValue().getIdPista(),
+              Reserva r = new Reserva(id.getText(), idSocio.getValue(), idPista.getValue(),
                       fecha.getValue(), t, duracion.getValue(), Double.parseDouble(precio.getText()));
-           //     boolean ok = club.crearReserva(r);
+                club.crearReserva(r);
+                showInfo("Reserva realizada com éxito");
             } catch (Exception ex) {
                 showError(ex.getMessage());
             }
